a8b06002e609f7bf4a2a227c6d45f045e622cf9f
oae26 <oae26@njit.edu>
2024-10-29 17:24:43 -0400

    fixed problems being caused by github actions

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -1,14 +1,14 @@
 """This function is in charge of testing commands"""
 import pytest
-from app import App
+from app import Application
 
 def test_app_exit_command(capfd, monkeypatch):
     """Test that the REPL correctly handles the 'exit' command."""
     inputs = iter(['exit'])
     monkeypatch.setattr('builtins.input', lambda _: next(inputs))
-    app = App()
+    app = Application()
     with pytest.raises(SystemExit) as e:
-        app.start()
+        app.launch()
     # Check that the exit was graceful with the correct exit code
     assert e.value.code == 0, "The app did not exit as expected"
   
